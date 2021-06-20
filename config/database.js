73bf3add669d478105125d7f54d9a7dae823e6a3
อธิวัตร เชยโนนสูง <atiwat.cns@gmail.com>
2021-06-20 16:15:49 +0700

    add config.yml

--- config/database.js
+++ config/database.js
@@ -1,89 +1,34 @@
-'use strict'
+"use strict";
 
-const Env = use('Env')
-const Helpers = use('Helpers')
-const Url = require('url-parse')
-const CLEARDB_DATABASE_URL = new Url(Env.get('CLEARDB_DATABASE_URL'))
+const Env = use("Env");
+
+const Helpers = use("Helpers");
+const URL = require("url-parse");
+const PROD_MYSQL_DB = new URL(Env.get("CLEARDB_DATABASE_URL"));
 
 module.exports = {
-  /*
-  |--------------------------------------------------------------------------
-  | Default Connection
-  |--------------------------------------------------------------------------
-  |
-  | Connection defines the default connection settings to be used while
-  | interacting with SQL databases.
-  |
-  */
-  connection: {
-    host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
-    port: Env.get('DB_PORT', ''),
-    user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
-    password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
-    database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
-} ,
+  connection: Env.get("DB_CONNECTION", "sqlite"),
 
-  /*
-  |--------------------------------------------------------------------------
-  | Sqlite
-  |--------------------------------------------------------------------------
-  |
-  | Sqlite is a flat file database and can be good choice under development
-  | environment.
-  |
-  | npm i --save sqlite3
-  |
-  */
   sqlite: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: Helpers.databasePath(`${Env.get('DB_DATABASE', 'development')}.sqlite`)
+      filename: Helpers.databasePath(
+        `${Env.get("DB_DATABASE", "adonis")}.sqlite`
+      )
     },
     useNullAsDefault: true,
-    debug: Env.get('DB_DEBUG', false)
+    debug: Env.get("DB_DEBUG", false)
   },
 
-  /*
-  |--------------------------------------------------------------------------
-  | MySQL
-  |--------------------------------------------------------------------------
-  |
-  | Here we define connection settings for MySQL database.
-  |
-  | npm i --save mysql
-  |
-  */
   mysql: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      host: Env.get("DB_HOST", PROD_MYSQL_DB.host),
+      port: Env.get("DB_PORT", ""),
+      user: Env.get("DB_USER", PROD_MYSQL_DB.username),
+      password: Env.get("DB_PASSWORD", PROD_MYSQL_DB.password),
+      database: Env.get("DB_DATABASE", PROD_MYSQL_DB.pathname.substr(1))
     },
-    debug: Env.get('DB_DEBUG', false)
-  },
-
-  /*
-  |--------------------------------------------------------------------------
-  | PostgreSQL
-  |--------------------------------------------------------------------------
-  |
-  | Here we define connection settings for PostgreSQL database.
-  |
-  | npm i --save pg
-  |
-  */
-  pg: {
-    client: 'pg',
-    connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    },
-    debug: Env.get('DB_DEBUG', false)
+    debug: Env.get("DB_DEBUG", false)
   }
-}
+};
